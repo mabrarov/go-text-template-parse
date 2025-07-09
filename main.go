aace831b14314fee1a8a4269f1d9ed5f73e829dd
Marat Abrarov <abrarov@gmail.com>
2025-07-10 00:51:35 +0300

    Parsing action nodes and command nodes to extract field names from expressions.

--- main.go
+++ main.go
@@ -8,23 +8,33 @@ import (
 
 func main() {
 	t := template.Must(template.New("cooltemplate").Parse(`<h1>{{ .name }} {{ printf "%d" .age }}</h1>`))
-	fmt.Println(listTemplateFields(t))
+	fmt.Println(listTemplateFields(t.Tree.Root))
 }
 
-func listTemplateFields(t *template.Template) []string {
-	return listNodeFields(t.Tree.Root)
-}
-
-func listNodeFields(node parse.Node) []string {
-	var res []string
-	if node.Type() == parse.NodeAction {
-		res = append(res, node.String())
-	}
-
-	if ln, ok := node.(*parse.ListNode); ok {
-		for _, n := range ln.Nodes {
-			res = append(res, listNodeFields(n)...)
+func listTemplateFields(node parse.Node) []string {
+	var ids []string
+	switch node.Type() {
+	case parse.NodeList:
+		listNode := node.(*parse.ListNode)
+		for _, n := range listNode.Nodes {
+			ids = append(ids, listTemplateFields(n)...)
 		}
+	case parse.NodeAction:
+		actionNode := node.(*parse.ActionNode)
+		if actionNode.Pipe != nil {
+			for _, cmdNode := range actionNode.Pipe.Cmds {
+				ids = append(ids, listTemplateFields(cmdNode)...)
+			}
+		}
+	case parse.NodeCommand:
+		commandNode := node.(*parse.CommandNode)
+		for _, node := range commandNode.Args {
+			ids = append(ids, listTemplateFields(node)...)
+		}
+	case parse.NodeField:
+		fieldNode := node.(*parse.FieldNode)
+		ids = append(ids, fieldNode.Ident...)
+
 	}
-	return res
+	return ids
 }
